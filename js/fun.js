fff047e66b0403cda84891faaf4e7aec346ac8ff
stegmanh <stegman.holden@gmail.com>
2016-02-17 21:01:51 -0800

    Allow updating

--- js/fun.js
+++ js/fun.js
@@ -1,8 +1,49 @@
+/*
+* Annie Tao, Holden Stegman
+* Februrary 17th, Info 474 Assignment 4
+*/
+// Object representing current baseline state (All boxes checked, etc)
+var baseline = {
+    "sex": ["male", "female"],
+    "age": [0, 80],
+    "pclass": ["1", "2", "3"]
+}
+
+// Object representing current state of selections
 var current = {
     "sex": ["male", "female"],
     "age": [0, 80],
     "pclass": ["1", "2", "3"]
 }
+// Create event listeners for all inputs on the page
+var checkboxes = Array.from(document.getElementsByTagName("input"));
+for (var i = 0; i < checkboxes.length; i++) {
+    var input = checkboxes[i];
+    input.addEventListener("change", function(e) {
+        var targetName = e.target.name;
+        if (current.hasOwnProperty(targetName)) {
+            if (targetName == "sex" || "pclass") {
+                if (e.target.checked) {
+                    current[targetName].push(e.target.value.toLowerCase());
+                } else {
+                    current[targetName].splice(current[targetName].indexOf(e.target.value.toLowerCase()), 1);
+                }
+                //console.log(current)
+                update()
+            }
+        }
+    })
+}
+
+
+/*
+TODO: CHANGE SIZE ON AGE
+*/
+
+// Function that listens and updates slider input
+
+// Declare doubounce update
+var debouceUpdate = debounce(update, 500);
 
 $(function () {
     $("#slider-range").slider({
@@ -13,12 +54,40 @@ $(function () {
         slide: function (event, ui) {
             current["age"][0] = ui.values[0], current["age"][1] = ui.values[1];
             $("#amount").val(ui.values[0] + " - " + ui.values[1]);
+            debouceUpdate();
         }
     });
     $("#amount").val($("#slider-range").slider("values", 0) +
         " - " + $("#slider-range").slider("values", 1));
 });
 
+// Set up reset button
+document.getElementById("resetBtn").addEventListener("click", function() {
+    console.log("Resetting..");
+    current = JSON.parse(JSON.stringify(baseline));
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 80,
+        values: [0, 80],
+        slide: function (event, ui) {
+            current["age"][0] = ui.values[0], current["age"][1] = ui.values[1];
+            $("#amount").val(ui.values[0] + " - " + ui.values[1]);
+            debouceUpdate();
+        }
+    });
+    $("#amount").val($("#slider-range").slider("values", 0) +
+        " - " + $("#slider-range").slider("values", 1));
+    for (var i = 0; i < checkboxes.length; i++) {
+        var input = checkboxes[i];
+        if (input.type == 'checkbox') {
+            input.checked = true;
+        }
+    }
+    update();
+})
+
+// Height, Weight, Page info variables
 var margin = {
     top: 0,
     right: 0,
@@ -28,6 +97,7 @@ var margin = {
 width = 900- margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
+// Chart variables
 var n = 1309,
     m = 3,
     padding = 7,
@@ -35,8 +105,12 @@ var n = 1309,
     color = d3.scale.category10().domain(d3.range(m)),
     x = d3.scale.ordinal().domain(d3.range(m)).rangePoints([0, width], 1);
 
-
+// nodesCopy holds a deep copy of nodes
+// Nodes holds each node to be render (Put on screen)
+var nodesCopy = [];
 var nodes = [];
+
+// Load CSV, load nodes and nodesCopy, call initial render
 d3.csv("../titanic3.csv", function(err, data) {
     for (var j = 0; j < data.length; j++) {
         //create node
@@ -45,17 +119,62 @@ d3.csv("../titanic3.csv", function(err, data) {
 
         nodes.push({
             sex: data[j].sex,
+            age: data[j].age,
+            pclass: data[j].pclass,
+            name: data[j].name,
             radius: 5,
             cx: x(i),
             cy: height / 2,
-        })
+        });
+        nodesCopy.push({
+            sex: data[j].sex,
+            age: data[j].age,
+            pclass: data[j].pclass,
+            name: data[j].name,
+            radius: 5,
+            cx: x(i),
+            cy: height / 2,
+        });
     }
+    update();
     //console.log(nodes);
 });
+// Declare svg and circle as globals for easy access
 var circle;
-setTimeout(function() {
-    console.log(nodes);
+var svg;
 
+// Basic function takes nodes and displays them on the chart. Should only called on initial load
+// or called when resetting the page
+// Function that handles any updates that the user makes
+function update(amount) {
+    nodes = JSON.parse(JSON.stringify(nodesCopy));
+    nodes = nodes.filter(function (n) {
+        for (var prop in current) {
+            if (current.hasOwnProperty(prop)) {
+                if (current[prop].indexOf(n[prop]) == -1) {
+                    if (prop == "age") {
+                        if (n["age"] < current["age"][0] || n["age"] > current["age"][1]) {
+                            return false;
+                        }
+                    } else {
+                        console.log(current[prop], n[prop], "should be returning false..");
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    });
+    // Useful for stats?
+    console.log(nodes.length, "nodes length");
+    console.log(nodesCopy.length, "nodesCopy length");
+    // Update stats here
+    $('#passengerCount').text(function() {
+        var p = nodes.length == 1 ? "passenger" : "passengers";
+        return nodes.length + " " + p;
+    });
+    // End Update Stats
+    $('#chart').empty();
     var force = d3.layout.force()
         .nodes(nodes)
         .size([width, height])
@@ -64,7 +183,7 @@ setTimeout(function() {
         .on("tick", tick)
         .start();
 
-    var svg = d3.select("#chart").append("svg")
+    svg = d3.select("#chart").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -75,41 +194,43 @@ setTimeout(function() {
         .enter()
         .append("circle")
         .attr("r", function (d) {
-        return d.radius;
-    })
-        .attr("fill", function(d) {
-           if (d.sex == "female") {
-               return "#FF90AF";
-           } else {
-               return "#22ABCE";
-           }
+            //console.log(d);
+            return d.radius;
+        })
+        .attr("fill", function (d) {
+            if (d.sex == "female") {
+                return "#FF90AF";
+            } else {
+                return "#22ABCE";
+            }
+        })
+        .on("mouseover", function (d) {
+            $('#hoverHelpMessage').hide();
+            $('#passengerInfo').show();
+            $('#passengerName').text(d.name);
+            $('#passengerAge').text(d.age);
+            $('#passengerSex').text(function() {
+                return d.sex.split('')[0].toUpperCase() + d.sex.split('').splice(1).join('');
+            });
+        })
+        .on("mouseout", function (d) {
+            $('#hoverHelpMessage').show();
+            $('#passengerInfo').hide();
+            console.log("Good bye!");
         })
         .call(force.drag);
+}
 
-}, 500);
-// var nodes = d3.range(n).map(function () {
-//     var i = Math.floor(Math.random() * m), //color
-//         v = (i + 1) / m * -Math.log(Math.random()); //value
-//     return { //add each person fields here
-//         //classGroup = 1
-//         //age = 1
-//         radius: 6,
-//         color: color(i),
-//         cx: x(i),
-//         cy: height / 2,
-//     };
-
-// });
-
+// Function for gravity chart that control gravity, ticks, collissions
 function tick(e) {
     circle.each(gravity(.4 * (e.alpha/3))) 
         .each(collide(.35))
         .attr("cx", function (d) {
-        return d.x;
-    })
+            return d.x;
+        })
         .attr("cy", function (d) {
-        return d.y;
-    });
+            return d.y;
+        });
 }
 
 // Move nodes toward cluster focus.
@@ -147,3 +268,20 @@ function collide(alpha) {
         });
     };
 }
+
+// Helper debouce function for efficiency!!
+// Taken from: https://davidwalsh.name/javascript-debounce-function
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
